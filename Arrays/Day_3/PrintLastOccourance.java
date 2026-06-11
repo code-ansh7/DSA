0e70057f6686576b003948f9f7ba6b17386a9049
Ansh Rastogi <anshrastogi9236920967@gmail.com>
2026-06-11 16:27:33 +0545

    Add PrintLastOccurrence class to find last occurrence

---
+++ Arrays/Day_3/PrintLastOccourance.java
@@ -0,0 +1,31 @@
+ import java.util.*;
+public class PrintLastOccourance {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter array size: ");
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        System.out.print("Enter array elements: ");
+        for (int idx = 0; idx < arr.length; idx++) {
+            arr[idx] = sc.nextInt();            
+        }
+
+        System.out.print("Enter element to Search: ");
+        int target = sc.nextInt();
+
+        boolean found = false;
+        int index = -1;
+        for (int idx = 0; idx < arr.length; idx++) {
+            if(arr[idx] == target){
+                found = true;
+                index = idx;
+             }            
+        }
+         if(found){
+            System.out.println(target + " Found in " + index + "th Index!");
+        }
+        else{
+            System.out.println(target + " Not found in array!");
+        }    
+    }
+}
